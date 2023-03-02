c0bfb341b82a2dc298a91373024ec1890a28ed28
galib-shoaib <galibshoaib2001@gmail.com>
2023-03-03 02:34:00 +0600

    maximum element in a list

---
+++ maxelmnt.py
@@ -0,0 +1,6 @@
+number=[10,9,6,5,4,3,6,2]
+max_n=number[0]
+for n in number:
+    if n>max_n:
+        max_n=n
+print(max_n)
---
+++ nthsum.py
@@ -0,0 +1,6 @@
+sum=0
+
+for num in range(1,6,2):
+    sum=sum+num
+print(sum)
+
